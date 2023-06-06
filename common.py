d367641fc7589235b379b1da4b9b3ff1f27a7c25
Marc Hulsman <m.hulsman@tudelft.nl>
2023-06-06 10:07:18 +0200

    Missing import

--- common.py
+++ common.py
@@ -5,6 +5,7 @@ from collections import OrderedDict
 import read_samples
 from read_samples import PROTOCOLS
 import utils
+import yaml
 
 chr = ['chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chr20', 'chr21', 'chr22', 'chrX', 'chrY']
 main_chrs = ['chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chr20', 'chr21', 'chr22', 'chrX', 'chrY']
